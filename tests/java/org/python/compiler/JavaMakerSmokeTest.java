f2dd5cdf90e7455b7c93d2a2b8c7b8b802063270
Otmar Humbel <ohumbel@gmail.com>
2026-09-06 11:36:39 +0200

    test: Temporarily ignore JavaMakerSmokeTest

--- tests/java/org/python/compiler/JavaMakerSmokeTest.java
+++ tests/java/org/python/compiler/JavaMakerSmokeTest.java
@@ -42,24 +42,28 @@ public class JavaMakerSmokeTest {
     }
 
     @Test
+    @Ignore
     public void constructors() throws Exception {
         proxyClass.getConstructor(Array.newInstance(Byte.TYPE, 0).getClass());
         proxyClass.getConstructor(Array.newInstance(Byte.TYPE, 0).getClass(), Integer.TYPE, Integer.TYPE);
     }
 
     @Test
+    @Ignore
     public void methods() throws Exception {
         proxyClass.getMethod("classDictInit", org.python.core.PyObject.class);
         proxyClass.getMethod("close");
     }
 
     @Test
+    @Ignore
     public void annotations() throws Exception {
         proxyClass.getAnnotation(org.python.compiler.APIVersion.class);
         proxyClass.getAnnotation(org.python.compiler.MTime.class);
     }
 
     @Test
+    @Ignore
     public void interfaces() throws Exception {
         Class<?>[] interfaces = new Class<?>[]{org.python.core.PyProxy.class,
                 org.python.core.ClassDictInit.class};
