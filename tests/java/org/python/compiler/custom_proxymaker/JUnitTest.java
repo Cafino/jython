3f49350c1db040940a4d74ac4e822efe676d6e48
Otmar Humbel <ohumbel@gmail.com>
2026-09-07 20:20:37 +0200

    Enable JUnitTest again
    Knowing that it will fail...

--- tests/java/org/python/compiler/custom_proxymaker/JUnitTest.java
+++ tests/java/org/python/compiler/custom_proxymaker/JUnitTest.java
@@ -6,14 +6,12 @@ package org.python.compiler.custom_proxymaker;
  */
 
 import org.junit.Test;
-import org.junit.Ignore;
 import org.junit.runner.JUnitCore;
 import org.python.util.ProxyCompiler;
 
 public class JUnitTest {
 
     @Test
-    @Ignore
     public void testMethodSignatures() throws Exception {
         ProxyCompiler.compile("tests/python/custom_proxymaker/junit_test.py", "build/testclasses");
         JUnitCore.runClasses(Class.forName("custom_proxymaker.tests.JUnitTest"));
